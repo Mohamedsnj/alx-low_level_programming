f28e4bac9d88f77f074b48a2a4c5aef32965d1e0
Mohamed <medsanhaji101@gmail.com>
2023-08-28 06:31:15 -0700

    c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square matrix of integers
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, j, t1 = 0, t2 = 0;
+
+	for (i = 0; i <= (size * size); i = i + size + 1)
+		t1 = t1 + a[i];
+
+	for (j = size - 1; j <= (size * size) - size; j = j + size - 1)
+		t2 = t2 + a[j];
+	printf("%d, %d\n", t1, t2);
+}
