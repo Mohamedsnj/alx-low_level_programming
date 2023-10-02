24f341a50704ba0696e059970955f336dcad86fb
Mohamedsnj <medsanhaji101@gmail.com>
2023-10-02 11:14:38 -0700

    c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * create_file - creates a file.
+ *
+ * @text_content: is a NULL terminated string to write to the file
+ * @filename: is the name of the file to create
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int a, b, length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (length = 0; text_content[length];)
+			length++;
+	}
+
+	a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	b = write(a, text_content, length);
+
+	if (a == -1 || b == -1)
+		return (-1);
+
+	close(a);
+
+	return (1);
+}
